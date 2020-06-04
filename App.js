3b79f5ea74cdab81cac48004a12857919440b5fb
raghavasharma28 <raghava.sharma@icf.com>
2020-06-04 17:55:46 +0530

    inline styling

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder='Enter Text' style={{borderColor: '#000', borderWidth: 1, padding: 10, width: '80%'}}/>
+        <Button title='Add' />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
